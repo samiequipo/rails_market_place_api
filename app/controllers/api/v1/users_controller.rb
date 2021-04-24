115fd0b62e62d936d8c79795757b92914fae873b
samuel <samiequipo@gmail.com>
2021-04-24 07:46:11 -0400

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:update]
   
   def show
     render json: User.find(params[:id])
@@ -13,9 +14,21 @@ class Api::V1::UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-
+  
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+  
   private
     def user_params
       params.require(:user).permit(:email, :password)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
     end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -32,4 +32,16 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     
     assert_response :unprocessable_entity #422
   end
+  
+  test "should update user" do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+   
+    assert_response :success
+  end
+
+  test "should not update user when invalid params are sent" do
+    patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
+  
+    assert_response :unprocessable_entity
+  end
 end
