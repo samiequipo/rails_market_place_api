0bae2a3aa658ec743387c94368d9e03bd1f69b1f
samuel <samiequipo@gmail.com>
2021-04-24 06:44:09 -0400

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
+
+  # bycript
+  has_secure_password
 end
