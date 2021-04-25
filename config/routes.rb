5dc871e4dccc459655e35d69f71c79523d0332cd
samuel <samiequipo@gmail.com>
2021-04-25 13:19:38 -0400

    Adds the products create, update and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, except: [:new, :edit]  
       resources :tokens, only: [:create] 
+      resources :products, only: [:show, :index, :create, :update, :destroy] 
     end
   end
 end
