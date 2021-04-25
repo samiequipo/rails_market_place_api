9a7ae6110698f2a5b85336ca8ed0a4fc947c8ee1
samuel <samiequipo@gmail.com>
2021-04-25 11:29:54 -0400

    Restrict actions for unauthorized users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :tokens, only: [:create] 
-    end
-  end
-
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
       resources :users, except: [:new, :edit]  
+      resources :tokens, only: [:create] 
     end
   end
 end
--- test/controllers/concerns/authenticable_test.rb
+++ test/controllers/concerns/authenticable_test.rb
@@ -1,14 +1,14 @@
-class AuthenticableTest < ActionDispatch::IntegrationTest
-  class MockController
-    include Authenticable
-    attr_accessor :request
-    
-    def initialize
-      mock_request = Struct.new(:headers)
-      self.request = mock_request.new({})
-    end
+require 'test_helper'
+class MockController
+  include Authenticable
+  attr_accessor :request
+
+  def initialize
+    mock_request = Struct.new(:headers)
+    self.request = mock_request.new(header: {})
   end
-  
+end
+class AuthenticableTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
     @authentication = MockController.new
@@ -16,9 +16,10 @@ class AuthenticableTest < ActionDispatch::IntegrationTest
 
   test 'should get user from Authorization token' do
     @authentication.request.headers['Authorization'] = JsonWebToken.encode(user_id: @user.id)
+    assert_not_nil @authentication.current_user
     assert_equal @user.id, @authentication.current_user.id
   end
-  
+
   test 'should not get user from empty Authorization token' do
     @authentication.request.headers['Authorization'] = nil
     assert_nil @authentication.current_user
